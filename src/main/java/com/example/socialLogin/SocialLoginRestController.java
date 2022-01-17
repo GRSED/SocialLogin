d9e709bb6674cfac5961ce510f62a92d645ee779
junseong <junseong@unitsoft.kr>
2022-01-17 16:37:14 +0900

    kakao, google 추가

--- src/main/java/com/example/socialLogin/SocialLoginRestController.java
+++ src/main/java/com/example/socialLogin/SocialLoginRestController.java
@@ -10,30 +10,49 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import static com.example.socialLogin.Constant.*;
+
 @RestController
 @RequestMapping(value = "/login/oauth2/code")
 public class SocialLoginRestController {
 
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
-
     @GetMapping(value = "/naver")
-    public String code(@RequestParam("code") String code, @RequestParam("state") String state) {
-        JSONObject jsonObject = new JSONObject(requestAccessToken(generateAuthCodeRequest(code, state)).getBody());
+    public String naver(@RequestParam("code") String code, @RequestParam("state") String state) {
+        JSONObject jsonObject = new JSONObject(requestAccessTokenNaver(generateAuthCodeRequestNaver(code, state)).getBody());
         String accessToken = jsonObject.getString("access_token");
-        return requestProfile(generateProfileRequest(accessToken)).getBody();
+        return requestProfileNaver(generateProfileRequestNaver(accessToken)).getBody();
     }
 
-    private ResponseEntity<String> requestAccessToken(HttpEntity request) {
+    private HttpEntity<MultiValueMap<String, String>> generateAuthCodeRequestNaver(String code, String state) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", NAVER_CLIENT_ID);
+        params.add("client_secret", NAVER_CLIENT_SECRET);
+        params.add("code", code);
+        return new HttpEntity<>(params, headers);
+    }
+
+    private ResponseEntity<String> requestAccessTokenNaver(HttpEntity request) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.exchange(
                 "https://nid.naver.com/oauth2.0/token",
                 HttpMethod.POST,
                 request,
-                String.class);
+                String.class
+        );
     }
 
-    private ResponseEntity<String> requestProfile(HttpEntity request) {
+    private HttpEntity<MultiValueMap<String, String>> generateProfileRequestNaver(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        return new HttpEntity<>(headers);
+    }
+
+    private ResponseEntity<String> requestProfileNaver(HttpEntity request) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.exchange(
                 "https://openapi.naver.com/v1/nid/me",
@@ -42,22 +61,95 @@ public class SocialLoginRestController {
                 String.class);
     }
 
-    private HttpEntity<MultiValueMap<String, String>> generateProfileRequest(String accessToken) {
+    @GetMapping(value = "/kakao")
+    public String kakao(@RequestParam("code") String code) {
+        JSONObject jsonObject = new JSONObject(requestAccessTokenKakao(generateAuthCodeRequestKakao(code)).getBody());
+        String accessToken = jsonObject.getString("access_token");
+        return requestProfileKakao(generateProfileRequestKakao(accessToken)).getBody();
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> generateAuthCodeRequestKakao(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", KAKAO_CLIENT_ID);
+        params.add("client_secret", KAKAO_CLIENT_SECRET);
+        params.add("redirect_uri", "http://localhost:8080/login/oauth2/code/kakao");
+        params.add("code", code);
+        return new HttpEntity<>(params, headers);
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> generateProfileRequestKakao(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + accessToken);
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
         return new HttpEntity<>(headers);
     }
 
-    private HttpEntity<MultiValueMap<String, String>> generateAuthCodeRequest(String code, String state) {
+    private ResponseEntity<String> requestAccessTokenKakao(HttpEntity request) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                request,
+                String.class
+        );
+    }
+
+    private ResponseEntity<String> requestProfileKakao(HttpEntity request) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                request,
+                String.class);
+    }
+
+    @GetMapping(value = "/google")
+    public String google(@RequestParam("code") String code) {
+        JSONObject jsonObject = new JSONObject(requestAccessTokenGoogle(generateAuthCodeRequestGoogle(code)).getBody());
+        String accessToken = jsonObject.getString("access_token");
+        return requestProfileGoogle(generateProfileRequestGoogle(accessToken)).getBody();
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> generateAuthCodeRequestGoogle(String code) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        headers.add("Content-type","application/x-www-form-urlencoded");
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", CLIENT_ID);
-        params.add("client_secret", CLIENT_SECRET);
+        params.add("client_id", GOOGLE_CLIENT_ID);
+        params.add("client_secret", GOOGLE_CLIENT_SECRET);
+        params.add("redirect_uri", "http://localhost:8080/login/oauth2/code/google");
         params.add("code", code);
         return new HttpEntity<>(params, headers);
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> generateProfileRequestGoogle(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        return new HttpEntity<>(headers);
+    }
+
+    private ResponseEntity<String> requestAccessTokenGoogle(HttpEntity request) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(
+                "https://oauth2.googleapis.com/token",
+                HttpMethod.POST,
+                request,
+                String.class
+        );
+    }
+
+    private ResponseEntity<String> requestProfileGoogle(HttpEntity request) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(
+                "https://www.googleapis.com/oauth2/v2/userinfo",
+                HttpMethod.GET,
+                request,
+                String.class
+        );
     }
 }
